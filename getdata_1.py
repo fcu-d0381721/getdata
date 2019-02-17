8b3dc139a425fd7cd79b1d0cdbf5b181697e16fd
Josephine Lai <s292423@gmail.com>
2019-02-17 20:18:11 +0800

    遇到打不開的檔，會再次讀取

--- getdata_1.py
+++ getdata_1.py
@@ -19,63 +19,69 @@ minute_time = ''
 
 
 
-def parseXML(url,count,x):
+def parseXML(tree,count,x):
 
     temp = [()]
-    try:
-        sitemap = gzip.GzipFile(fileobj=BytesIO(url.content))
-        root = ET.parse(sitemap)
-        tree = root.getroot()
 
-        for infos in tree:
-            Infos = infos
+    while True:
+        try:
+            # sitemap = gzip.GzipFile(fileobj=BytesIO(url.content))
+            # root = ET.parse(sitemap)
+            # tree = root.getroot()
 
-            for info in Infos:
-                undivide_total_speed = 0
-                undivide_total_laneoccupy = 0
-                undivide_total_volume = 0
+            for infos in tree:
+                Infos = infos
 
-                for lane in info:
-                    divide_total_volume = 0
-                    now_speed = int(lane.attrib["speed"])
-                    now_laneoccupy = int(lane.attrib["laneoccupy"])
+                for info in Infos:
+                    undivide_total_speed = 0
+                    undivide_total_laneoccupy = 0
+                    undivide_total_volume = 0
 
-                    for cars in lane:
-                        divide_total_volume += int(cars.attrib["volume"])
-                        undivide_total_speed += now_speed * int(cars.attrib["volume"])
-                        undivide_total_laneoccupy += now_laneoccupy * int(cars.attrib["volume"])
-                        undivide_total_volume += int(cars.attrib["volume"])
-                        ###全部資料###
-                        # print("vdid: " + info.attrib["vdid"] + " status: " + info.attrib["status"] + " datacollecttime: " + info.attrib["datacollecttime"] + " vsrdir: " + lane.attrib["vsrdir"] + " vsrid: " + lane.attrib["vsrid"] + " speed: " + lane.attrib["speed"] + " laneoccupy: " + lane.attrib["laneoccupy"] + " carid: " + cars.attrib["carid"] + " volume: " + cars.attrib["volume"])
+                    for lane in info:
+                        divide_total_volume = 0
+                        now_speed = int(lane.attrib["speed"])
+                        now_laneoccupy = int(lane.attrib["laneoccupy"])
 
-
-                    divide_total_laneoccupy = Decimal(float(lane.attrib["laneoccupy"]) / 1.21).quantize(Decimal('0.00'))
-
-                    # 分車道
-                    if temp == [()]:
-                        # datacollecttime = datetime.datetime.strptime(info.attrib["datacollecttime"], "%Y/%m/%d %H:%M:%S")
-                        temp = [(info.attrib["vdid"], info.attrib["datacollecttime"], lane.attrib["vsrid"], lane.attrib["speed"], str(divide_total_laneoccupy), divide_total_volume*60)]
-
-                    else:
-                        # datacollecttime = datetime.datetime.strptime(info.attrib["datacollecttime"], "%Y/%m/%d %H:%M:%S")
-                        temp = temp + [(info.attrib["vdid"], info.attrib["datacollecttime"], lane.attrib["vsrid"], lane.attrib["speed"], str(divide_total_laneoccupy), divide_total_volume*60)]
-                    # print("vdid: " + info.attrib["vdid"] + " datacollecttime: " + info.attrib["datacollecttime"] + " vsrid: " + lane.attrib["vsrid"] + " speed: " + lane.attrib["speed"] + " laneoccupy: " + str(divide_total_laneoccupy) + " volume: " + str(divide_total_volume*60))
+                        for cars in lane:
+                            divide_total_volume += int(cars.attrib["volume"])
+                            undivide_total_speed += now_speed * int(cars.attrib["volume"])
+                            undivide_total_laneoccupy += now_laneoccupy * int(cars.attrib["volume"])
+                            undivide_total_volume += int(cars.attrib["volume"])
+                            ###全部資料###
+                            # print("vdid: " + info.attrib["vdid"] + " status: " + info.attrib["status"] + " datacollecttime: " + info.attrib["datacollecttime"] + " vsrdir: " + lane.attrib["vsrdir"] + " vsrid: " + lane.attrib["vsrid"] + " speed: " + lane.attrib["speed"] + " laneoccupy: " + lane.attrib["laneoccupy"] + " carid: " + cars.attrib["carid"] + " volume: " + cars.attrib["volume"])
 
 
-                # if undivide_total_volume != 0:
-                #     undivide_total_speed = Decimal(undivide_total_speed / undivide_total_volume).quantize(Decimal('0.00'))
-                #     undivide_total_laneoccupy = Decimal((undivide_total_laneoccupy / undivide_total_volume) / 1.21).quantize(Decimal('0.00'))
+                        divide_total_laneoccupy = Decimal(float(lane.attrib["laneoccupy"]) / 1.21).quantize(Decimal('0.00'))
 
-                # 不分車道
-                # print("vdid: " + info.attrib["vdid"] + " datacollecttime: " + info.attrib["datacollecttime"] + " speed: " + str(undivide_total_speed) + " laneoccupy: " + str(undivide_total_laneoccupy) + " volume: " + str(undivide_total_volume*60))
+                        # 分車道
+                        if temp == [()]:
+                            # datacollecttime = datetime.datetime.strptime(info.attrib["datacollecttime"], "%Y/%m/%d %H:%M:%S")
+                            temp = [(info.attrib["vdid"], info.attrib["datacollecttime"], lane.attrib["vsrid"], lane.attrib["speed"], str(divide_total_laneoccupy), divide_total_volume*60)]
 
-        print("^___^")
-        temp = tuple(temp)
-        temp = str(temp)
-        temp = temp[1:-1]
-        x.insert_divide(temp)
-    except:
-        print('----------------------------------------------------------------------------' + end_date + " " + minute_time)
+                        else:
+                            # datacollecttime = datetime.datetime.strptime(info.attrib["datacollecttime"], "%Y/%m/%d %H:%M:%S")
+                            temp = temp + [(info.attrib["vdid"], info.attrib["datacollecttime"], lane.attrib["vsrid"], lane.attrib["speed"], str(divide_total_laneoccupy), divide_total_volume*60)]
+                        # print("vdid: " + info.attrib["vdid"] + " datacollecttime: " + info.attrib["datacollecttime"] + " vsrid: " + lane.attrib["vsrid"] + " speed: " + lane.attrib["speed"] + " laneoccupy: " + str(divide_total_laneoccupy) + " volume: " + str(divide_total_volume*60))
+
+
+                    # if undivide_total_volume != 0:
+                    #     undivide_total_speed = Decimal(undivide_total_speed / undivide_total_volume).quantize(Decimal('0.00'))
+                    #     undivide_total_laneoccupy = Decimal((undivide_total_laneoccupy / undivide_total_volume) / 1.21).quantize(Decimal('0.00'))
+
+                    # 不分車道
+                    # print("vdid: " + info.attrib["vdid"] + " datacollecttime: " + info.attrib["datacollecttime"] + " speed: " + str(undivide_total_speed) + " laneoccupy: " + str(undivide_total_laneoccupy) + " volume: " + str(undivide_total_volume*60))
+
+            print("^___^")
+            temp = tuple(temp)
+            temp = str(temp)
+            temp = temp[1:-1]
+            x.insert_divide(temp)
+        except Exception as e:
+            print(e)
+            print('----------------------------------------------------------------------------' + end_date + " " + minute_time)
+            continue
+        break
+
 
 
 
@@ -108,13 +114,23 @@ def main():
             minute_time = StartTime + datetime.timedelta(minutes=minute)
             minute_time = minute_time.strftime("%H%M")
             minute_time = str(minute_time)
-            headers = {'user-agent': '"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'}
-            result = requests.get("http://tisvcloud.freeway.gov.tw/history/vd/" + end_date + "/vd_value_" + minute_time + ".xml.gz",headers = headers)
-            result.encoding = 'utf8'
-            # jsonData +=\
-            count = count + 1
-            parseXML(result, count,x)
-            result.close()
+            while True:
+                try:
+                    headers = {'user-agent': '"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'}
+                    result = requests.get("http://tisvcloud.freeway.gov.tw/history/vd/" + end_date + "/vd_value_" + minute_time + ".xml.gz",headers = headers)
+                    result.encoding = 'utf8'
+                    # jsonData +=\
+                    count = count + 1
+                    sitemap = gzip.GzipFile(fileobj=BytesIO(result.content))
+                    root = ET.parse(sitemap)
+                    tree = root.getroot()
+                    parseXML(tree, count, x)
+                    result.close()
+                except Exception as e:
+                    print(e)
+                    print('----------------------------------------------------------------------------' + end_date + " " + minute_time)
+                    continue
+                break
         time.sleep(1)
         count = 0
 
